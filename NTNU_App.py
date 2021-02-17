84aecc2b16c959144f18ca7db6d200aeb293d326
AndrewLiu0725 <e5n41kb9n@gmail.com>
2021-02-17 20:42:03 +0800

    update the main application: implement reading text from HTBasic's window

--- NTNU_App.py
+++ NTNU_App.py
@@ -2,12 +2,17 @@ import sys
 import UI.AnalyticalParameters_UI
 import UI.Homepage_UI
 from PyQt5 import QtCore, QtGui, QtWidgets
-import random
 import logging
 import pywinauto
+import time
+import numpy as np 
+from PIL import Image
+from TextExtractor import TextExtractor
 import warnings
 warnings.simplefilter("ignore", UserWarning)
 sys.coinit_flags = 2
+
+DEBUG = 1
 
 """
 all the corss class methods must write in App() class 
@@ -31,15 +36,19 @@ class AnalyticalParameters(QtWidgets.QMainWindow, UI.AnalyticalParameters_UI.Ui_
 # ===============================================================================
 class App():
     def __init__(self):
-        # initilize
+        # initilization for GUI
         self.app = QtWidgets.QApplication(sys.argv)
         self.homepage = HomePage()
         self.analyticalparam = AnalyticalParameters()
         self.analyticalparam.TableContent = []
         self.analyticalparam_table_initialized = 0
 
+        # initialization for HTBasic
         app = pywinauto.application.Application(backend="uia").start(r"C:\Program Files (x86)\HTBwin10\HTBwin.exe")
         main_dlg = app["TransEra - HTBasic - [Untitled]"]
+        main = main_dlg.wrapper_object()
+        main.iface_transform.Move(0, 0) # move the window to top-left corner
+        main.iface_transform.Resize(600, 600) # change width and height
         pywinauto.keyboard.send_keys("^o")
         LoadBasic = main_dlg['Open File']
         HTBasic_filepath = "D:\ARARSTAR\BLP3600\COLLECT\ARAUTO-3600m2020C"
@@ -63,46 +72,68 @@ class App():
     # ===============================================================================
     def enter_analyticalparam(self):
         self.HTBasic_dlg.type_keys("77{ENTER}")
+        
         if not self.analyticalparam_table_initialized:
-            a = [random.randint(1, 100) for _ in range(14)] # change here
-            self.analyticalparam_tableSetup(a)
+            # take screentshot from console if first enter
+            time.sleep(0.5)
+            Image.fromarray((np.asarray(self.HTBasic_dlg.capture_as_image()).copy())[135:360, 480:520]).save("./Figures/screenshot.png")
+            status = TextExtractor("./Figures/screenshot.png")
+            if DEBUG: print("The status in AP is:\n",status)
+            self.analyticalparam_tableSetup(status)
             self.analyticalparam_table_initialized = 1
-        self.homepage.hide()
         self.analyticalparam.show()
-        self.analyticalparam.tableWidget.itemChanged.connect(self.analyticalparam_setting_change) 
+        self.homepage.hide()
+        self.analyticalparam.tableWidget.itemChanged.connect(self.analyticalparam_parameter_change) # detect cell change
 
     def exit_analyticalparam(self):
-        self.analyticalparam.hide()
+        self.HTBasic_dlg.type_keys("0{ENTER}")
         self.homepage.show()
+        self.analyticalparam.hide()
+        
 
     def analyticalparam_tableSetup(self, dafault_value):
         # initialize the table (read the output from HTBasic)
-        self.analyticalparam.TableContent = []
+        self.analyticalparam.TableContent = [] # a list of string
         for i in range(self.analyticalparam.tableWidget.rowCount()):
             val =  dafault_value[i]
-            item = QtWidgets.QTableWidgetItem(str(val))
+            item = QtWidgets.QTableWidgetItem(val)
             self.analyticalparam.TableContent.append(val)
             self.analyticalparam.tableWidget.setItem(i, 0, item)
 
 
-    def analyticalparam_setting_change(self):
+    def analyticalparam_parameter_change(self):
         for i in range(self.analyticalparam.tableWidget.rowCount()):
             show_warning = 0
-            try:
-                val = int(self.analyticalparam.tableWidget.item(i, 0).text())
-                if val <= 0:
-                    show_warning = 1
-                else:
-                    if val != self.analyticalparam.TableContent[i]:
-                        self.analyticalparam.TableContent[i] = val
+            content = self.analyticalparam.tableWidget.item(i, 0).text()
 
-            except:
-                show_warning = 1
+            # check if the typed parameter is invalid
+            if i < 7: # typed parameter must be number
+                warning_msg = "Please key in positive integer!"
+                try:
+                    if int(content) <= 0:
+                        show_warning = 1
+
+                except: # not number
+                    show_warning = 1
+
+            else: # typed parameter must be yes or no
+                warning_msg = "Please key in either yes or no!"
+                if (content != "no") and (content != "yes"):
+                    show_warning = 1 
+
             
             if show_warning:
-                self.warningPopup("Please key in positive integer!")
+                self.warningPopup(warning_msg)
                 item = QtWidgets.QTableWidgetItem(str(self.analyticalparam.TableContent[i]))
                 self.analyticalparam.tableWidget.setItem(i, 0, item)
+
+            else:
+                if content != self.analyticalparam.TableContent[i]: # parameter changed
+                    self.analyticalparam.TableContent[i] = content # change the table content in GUI class
+                    # send signal to HTBasic
+                    self.HTBasic_dlg.type_keys("{}{ENTER}".format(i+1)) # which parameter
+                    self.HTBasic_dlg.type_keys("{}{ENTER}".format(content)) # assign value
+
 
     # warning message box
     def warningPopup(self, waring_msg):
--- TextExtractor.py
+++ TextExtractor.py
@@ -4,113 +4,118 @@ import imagehash
 import pickle
 
 
+def TextExtractor(filename):
+    # load the reference pictures library
+    with open("reference_dict.pickle", 'rb') as handle:
+        reference_pic_lib = pickle.load(handle)
 
-# load the reference pictures library
-with open("reference_dict.pickle", 'rb') as handle:
-    reference_pic_lib = pickle.load(handle)
+    # load the screenshot
+    img = np.asarray((Image.open(filename)).convert("L")).copy()
+    (max_y, max_x) = img.shape
 
-# load the screenshot
-img = np.asarray((Image.open("TestText.png")).convert("L")).copy()
-(max_y, max_x) = img.shape
-
-# covert image to pure black and white (all non-black pixel set to white)
-brightness_threshold = 40
-for x in range(max_x):
-    for y in range(max_y):
-        if img[y, x] > brightness_threshold: 
-            img[y, x] = 255 # set to white
-        else: 
-            img[y, x] = 0 # set to black
+    # covert image to pure black and white (all non-black pixel set to white)
+    brightness_threshold = 40
+    for x in range(max_x):
+        for y in range(max_y):
+            if img[y, x] > brightness_threshold: 
+                img[y, x] = 255 # set to white
+            else: 
+                img[y, x] = 0 # set to black
 
 
-# extract the text from the screenshot
-text = ""
-Bottom = -1
-
-while True:
-    FoundTop = 0
-    Top = Bottom + 1
-
-    # find top row for current line
-    while (Top < max_y):
-        if np.any(img[Top, :]):
-            FoundTop = 1
-            break
-        else:
-            Top += 1
-    
-    # break if there is no line remanined
-    if FoundTop:
-        Bottom = Top + 1
-    else:
-        break
-
-    # find bottom row for current line
-    while (Bottom < max_y):
-        if np.any(img[Bottom, :]):
-            Bottom += 1
-        else:
-            break
-    Bottom -= 1 # last row which is not all black
-
-    # parse the current line
-    Right = -1
+    # extract the text from the screenshot
+    text = ""
+    Bottom = -1
 
     while True:
-        FoundLeft = 0
-        Left = Right + 1
+        FoundTop = 0
+        Top = Bottom + 1
 
-        # find left column for current character
-        while (Left < max_x):
-            if np.any(img[Top:Bottom, Left]):
-                FoundLeft = 1
+        # find top row for current line
+        while (Top < max_y):
+            if np.any(img[Top, :]):
+                FoundTop = 1
                 break
             else:
-                Left += 1
-
-        # break if there is no character remained
-        if FoundLeft:
-            Right = Left + 1
+                Top += 1
+        
+        # break if there is no line remanined
+        if FoundTop:
+            Bottom = Top + 1
         else:
             break
 
-        # find right column for current character
-        while (Right < max_x):
-            if np.any(img[Top:Bottom, Right]):
-                Right += 1
+        # find bottom row for current line
+        while (Bottom < max_y):
+            if np.any(img[Bottom, :]):
+                Bottom += 1
+            else:
+                break
+        Bottom -= 1 # last row which is not all black
+
+        # parse the current line
+        Right = -1
+
+        while True:
+            FoundLeft = 0
+            Left = Right + 1
+
+            # find left column for current character
+            while (Left < max_x):
+                if np.any(img[Top:Bottom, Left]):
+                    FoundLeft = 1
+                    break
+                else:
+                    Left += 1
+
+            # break if there is no character remained
+            if FoundLeft:
+                Right = Left + 1
             else:
                 break
 
-        Right -= 1
+            # find right column for current character
+            while (Right < max_x):
+                if np.any(img[Top:Bottom, Right]):
+                    Right += 1
+                else:
+                    break
 
-        # refine the top and bottom row for this char
-        Top_refined = Top
+            Right -= 1
 
-        while (Top_refined <= Bottom):
-            if np.any(img[Top_refined, Left:Right]):
-                break
-            else:
-                Top_refined += 1
+            # refine the top and bottom row for this char
+            Top_refined = Top
 
-        Bottom_refined = Top_refined + 1
+            while (Top_refined <= Bottom):
+                if np.any(img[Top_refined, Left:Right]):
+                    break
+                else:
+                    Top_refined += 1
 
-        while (Bottom_refined <= Bottom):
-            if np.any(img[Bottom_refined, Left:Right]):
-                Bottom_refined += 1
-            else:
-                break
+            Bottom_refined = Top_refined + 1
 
-        Bottom_refined -= 1 # last row which is not all black
+            while (Bottom_refined <= Bottom):
+                if np.any(img[Bottom_refined, Left:Right]):
+                    Bottom_refined += 1
+                else:
+                    break
 
-        # recongnize the current char
-        hash_value = imagehash.average_hash(Image.fromarray(img[Top_refined:Bottom_refined, Left:Right]))
+            Bottom_refined -= 1 # last row which is not all black
 
-        min_hash_diff = 100
-        for char in reference_pic_lib.keys():
-            for i in range(len(reference_pic_lib[char])):
-                if abs(hash_value - reference_pic_lib[char][i]) < min_hash_diff:
-                    min_hash_diff = abs(hash_value - reference_pic_lib[char][i])
-                    most_like_char = char
-        text += most_like_char
-    text += "\n"
-print(text)
+            # recongnize the current char
+            hash_value = imagehash.average_hash(Image.fromarray(img[Top_refined:Bottom_refined, Left:Right]))
+
+            min_hash_diff = 100
+            for char in reference_pic_lib.keys():
+                for i in range(len(reference_pic_lib[char])):
+                    if abs(hash_value - reference_pic_lib[char][i]) < min_hash_diff:
+                        min_hash_diff = abs(hash_value - reference_pic_lib[char][i])
+                        most_like_char = char
+            text += most_like_char
+        text += "\n"
+    
+    return text
+
+
+if __name__ == "__main__":
+    print(TextExtractor("TestText.png"))
