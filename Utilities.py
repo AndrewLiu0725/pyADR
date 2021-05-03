792578e09ee7a6dff82b7d903387a8f18a15a6e1
AndrewLiu0725 <e5n41kb9n@gmail.com>
2021-05-03 11:34:21 +0800

    add outlier remover

--- Utilities.py
+++ Utilities.py
@@ -19,6 +19,9 @@ def calculateT0(filepath, plot):
     if plot:
         fig, axs = plt.subplots(2, 3, figsize = (12,8))
 
+    outlier_count = np.zeros(numCycle)
+
+    # go over Ar 36 to 40
     for i in range(5):
         t = v_t[i, :, 1]
         v = v_t[i, :, 0]
@@ -26,15 +29,45 @@ def calculateT0(filepath, plot):
         # linear regression 
         linear_model = np.polyfit(t, v, 1)
         linear_model_fn = np.poly1d(linear_model)
-        T0[i] = linear_model[1]
+
+        # recognize the outliers
+        baseline = np.std(np.abs(v - linear_model_fn(t))) # std of the error
+        for j in range(numCycle):
+            if np.abs(v[j] - linear_model_fn(t[j])) > baseline:
+                outlier_count[j] += 1
 
         if plot:
             axs[i//3, i%3].plot(t, v, marker = 'o', label = "experiment data")
-            axs[i//3, i%3].plot(t, linear_model_fn(t), label = "fitted line")
-            axs[i//3, i%3].legend()
-
-            axs[i//3, i%3].set_title("Ar {}\n{} = {} ".format(i+36, r'$T_{0}$', linear_model[1]))
+            axs[i//3, i%3].plot(t, linear_model_fn(t), linestyle = '--', label = "fitted line")
             axs[i//3, i%3].set(xlabel = "t (sec)", ylabel = "mV")
+            
+    # remove outlier
+    valid_indices = np.where(outlier_count < 2.5)[0]
+    outlier_indices = np.where(outlier_count > 2.5)[0]
+
+    for i in range(5):
+        t = v_t[i, valid_indices, 1]
+        v = v_t[i, valid_indices, 0]
+        
+        if len(valid_indices) > 1:
+            # linear regression 
+            linear_model = np.polyfit(t, v, 1)
+            linear_model_fn = np.poly1d(linear_model)
+            T0[i] = linear_model[1]
+
+            if plot:
+                axs[i//3, i%3].plot(v_t[i, outlier_indices, 1], v_t[i, outlier_indices, 0], marker = 'x', markersize = 12, linestyle = 'None', color = 'r')
+                axs[i//3, i%3].plot(t, linear_model_fn(t), linestyle = '--', label = "fitted line\n(exclude outliers)")
+                axs[i//3, i%3].legend()
+                axs[i//3, i%3].set_title("Ar {}\n{} = {} ".format(i+36, r'$T_{0}$', linear_model[1]))
+
+        else:
+            if plot:
+                axs[i//3, i%3].plot(v_t[i, outlier_indices, 1], v_t[i, outlier_indices, 0], marker = 'x', markersize = 12, linestyle = 'None', color = 'r')
+                axs[i//3, i%3].legend()
+                axs[i//3, i%3].set_title("Ar {}".format(i+36))
+
+
 
     if plot:
         axs[1,2].axis('off')
@@ -74,4 +107,5 @@ def calculateMassRatio(mass, background):
 
 
 if __name__ == "__main__":
-    print(calculateMassRatio("./Data/AS20210429a", "./Data/pb20210429a"))
+    print(calculateT0("./Data/AS20210429a", 1))
+    #print(calculateMassRatio("./Data/AS20210429a", "./Data/pb20210429a"))
