6e4363b968451794cf4659392d12d42cc7afd1a9
AndrewLiu0725 <e5n41kb9n@gmail.com>
2021-07-23 18:53:12 +0800

    complete windows part

--- install.py
+++ install.py
@@ -15,9 +15,19 @@ appName = 'NTNU_DataReduction'
 filename = appName+'.bat' if IS_WINDOWS else appName+'.sh'
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+# write the file
 with open(filename, 'w') as icon:
+    if IS_WINDOWS:
+        icon.write('@echo off\n')
+        anaconda_folder = sys.executable[:-10]
+        icon.write('call {}Scripts\activate.bat {}\n'.format(anaconda_folder, anaconda_folder))
     icon.write('cd {}\n'.format(dir_path))
     icon.write('{} {}'.format(sys.executable, os.path.abspath('NTNU_DataReduction.py')))
-subprocess.call(['chmod', 'u+x', os.path.abspath(filename)])
+
+# set the permission
+if IS_WINDOWS:
+    subprocess.call(['CACLS', os.path.abspath(filename), '/e', '/p', 'Everyone:f'])
+else:
+    subprocess.call(['chmod', 'u+x', os.path.abspath(filename)])
 
 print('Installation Complete!')
